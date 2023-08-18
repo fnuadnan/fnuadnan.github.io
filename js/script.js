e8e7215386d0ed94e1ed12c575fc27c846133b18
fnuadnan <129648022+fnuadnan@users.noreply.github.com>
2023-08-18 02:43:39 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,56 +1,11 @@
-// toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+// This is javascript code that adds the class "scroll-top" to the body whenever the body is at the top of the page and removes it otherwise.
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+window.onscroll = onScroll;
 
-// scroll sections
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if (top >= offset && top < offset + height) {
-            // active navbar links
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-            // active sections for animation on scroll
-            sec.classList.add('show-animate');
-        }
-        // if want to use animation that repeats on scroll use this
-        else {
-            sec.classList.remove('show-animate');
-        }
-    });
-
-    // sticky header
-    let header = document.querySelector('header');
-
-    header.classList.toggle('sticky', window.scrollY > 100);
-
-    // remove toggle icon and navbar when click navbar links (scroll)
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-
-    // animation footer on scroll
-    let footer = document.querySelector('footer');
-    let scrollable = document.documentElement.scrollHeight - window.innerHeight;
-    let scrolled = window.scrollY;
-
-    if (Math.ceil(scrolled) === scrollable) {
-        footer.classList.add('show-animate');
-    }
-    else {
-        footer.classList.remove('show-animate');
-    }
-}
+function onScroll() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.body.classList.remove("scroll-top");
+  } else {
+    document.body.classList.add("scroll-top");
+  }
+} 
